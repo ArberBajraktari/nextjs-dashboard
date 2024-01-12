fb3177fc4cd1c6f6961370ce5b41e34136d4fe69
Arber <arberbajraktari02@gmail.com>
2024-01-12 22:15:25 +0100

    Exported page

---
+++ app/dashboard/training/[id]/sport/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+    title: 'Sport',
+  };
+  
+export default async function Page() {
+  return (
+    <div>
+        Hey
+    </div>
+  );
+}
